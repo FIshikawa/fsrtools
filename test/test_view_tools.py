ce16ae22c88f5fe87c3eaabf6404834270ae6cb8
F Ishikawa <fumihiro@exa.phys.s.u-tokyo.ac.jp>
2019-04-24 19:32:49 +0900

    create view tools test

---
+++ test/test_view_tools.py
@@ -0,0 +1,54 @@
+import shutil
+import json
+import os
+import pytest
+import fsrtools.view_tools as fsrview 
+import fsrtools.simulate_tools as fsrsimulate
+
+class CommandManagerTest(fsrsimulate.CommandManager):
+    def __init__(self):
+        super(fsrsimulate.CommandManager,self).__init__()
+        self._json_path = fsrsimulate._commands_json_file(test=True)
+        if(os.path.exists(self._json_path)):
+            commands_json = open(self._json_path)
+            self.command_data = json.load(commands_json)
+            self.command_name_list = list(self.command_data.keys())
+        else:
+            self.command_data = {}
+            self.command_name_list= []
+
+
+@pytest.fixture(scope='module')
+def set_data_for_test():
+    files_list = os.listdir('./test/')
+    command_manager = CommandManagerTest()
+    if('hello_world' in command_manager.command_name_list):
+        command_manager.remove_command('hello_world')
+    command_manager.add_command({'hello_world' : ['python','./test/hello_world.py','N_loop']})
+    command_manager.save()
+    log_file = 'test/log.dat'
+    parameter_json = 'test/parameter_test.json'
+    command_data = command_manager.command_data
+    fsrsimulate.operate_experiments(parameter_file=parameter_json,log_file=log_file,test_mode=False,command_data=command_data)
+    yield set_data_for_test
+    command_manager.save()
+    files_list_created = os.listdir('./test/')
+    for key in list(set(files_list_created) - set(files_list)):
+        try:
+            os.remove(os.path.join('./test',key))
+        except OSError as now_error:
+            shutil.rmtree(os.path.join('./test',key))
+
+
+def test_set_result_info(set_data_for_test):
+    top_directory = ''
+    with os.scandir('./test') as files:
+        for entry in files:
+            if '-' in entry.name and entry.is_dir():
+                top_directory = entry.name
+                break
+    print(top_directory)
+    plot_manager = fsrview.PlotManager(top_directory=top_directory)
+    plot_manager.result_info(whole_info=True)
+
+
