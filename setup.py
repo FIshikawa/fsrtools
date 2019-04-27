6f570889bdedfdf3bd943e4285068a1bea3373db
Fumihiro <fumihiro@exa.phys.s.u-tokyo.ac.jp>
2019-04-27 11:58:32 +0900

    added requirement packagese

--- setup.py
+++ setup.py
@@ -1,12 +1,9 @@
 from setuptools import setup, find_packages
 import sys
-from fsrtools import __version__
 
 sys.path.append('./test')
 
-setup(name='fsrtools',
-      version=__version__,
-      description='F.Ishikawa made Package : manage numerical experiments',
+setup(
       packages=find_packages(),
       scripts=['bin/fsrview','bin/fsrsimulate'],
       setup_requires=['pytest-runner'],
