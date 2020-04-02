e2d92c5bd15b497ebe6c1ca2fbca8fab6d46fac9
Fumihiro Ishikawa <fumihiroishikawa@Fumihiros-MacBook-Air.local>
2020-04-02 21:41:59 +0900

    create tutorial file

--- tutorial/hello_world.py
+++ tutorial/hello_world.py
@@ -5,6 +5,9 @@ if __name__ == '__main__':
     argv = sys.argv
     argc = len(argv)
 
-    N_loop = int(argv[1])
+    if argc > 1:
+        N_loop = int(argv[1])
+    else:
+        N_loop = 1
     for i in range(N_loop):
         print('Hello World!')
