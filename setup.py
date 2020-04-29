75d3ace7be31d6e8ab472c9babb7d565f12c15cf
Fumihiro Ishikawa <fumihiroishikawa@Fumihiros-MacBook-Air.local>
2020-04-29 09:57:38 +0900

    change requirements declaration

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
+      # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 import sys
 
 sys.path.append('./test')
 
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
 setup(
       packages=find_packages(),
       scripts=['bin/fsrview','bin/fsrsimulate'],
       setup_requires=['pytest-runner'],
-      tests_require=['pytest']
+      tests_require=['pytest'],
+      long_description=readme,
+      license=license
       )
