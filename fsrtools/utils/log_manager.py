679845c7ad086a3462b8908dbfff07ab9859e672
Fumihiro Ishikawa <fumihiro@ai.phys.s.u-tokyo.ac.jp>
2020-05-06 14:10:40 +0900

    minorchange

--- fsrtools/utils/log_manager.py
+++ fsrtools/utils/log_manager.py
@@ -24,10 +24,11 @@ class LogManager:
     """
 
     def __init__(self,n_indent=0,log_file=None,cout_tag=False,silent=False):
-        if(cout_tag is False and log_file is None and silent is False):
-            raise ValueError('log_file is defined nesessarily when cout_tag is False.')
-        if(log_file is not None):
-            if(os.path.exists(log_file)):
+        if cout_tag is False and log_file is None and silent is False:
+            raise ValueError('log_file is defined nesessarily 
+                                when cout_tag is False.')
+        if log_file is not None:
+            if os.path.exists(log_file):
                 os.remove(log_file)
         self.log_file = log_file
         self.indent = n_indent
@@ -46,44 +47,44 @@ class LogManager:
         self.temp_indent = 0
 
     def add_indent(self,increase_number=0):
-        if(increase_number):
+        if increase_number:
             self.indent += increase_number
         else:
             self.indent += 1
         self.temp_indent = self.indent
 
     def decrease_indent(self,decrease_number=0):
-        if(decrease_number):
+        if decrease_number:
             self.indent -= decrease_number
         else:
             self.indent -= 1
         self.temp_indent = self.indent
 
     def __call__(self,sentence,log_file_temp=None,end='\n'):
-        if(isinstance(sentence,str)):
+        if isinstance(sentence,str):
             sentence = self.__indent() + sentence
         else:
             sentence = self.__indent() + '{}'.format(sentence)
-        if(log_file_temp is None and self.log_file is not None):
+        if log_file_temp is None and self.log_file is not None:
             f = open(self.log_file,'a')
             f.write(sentence + end)
             f.close()
-        elif(log_file_temp is not None):
+        elif log_file_temp is not None:
             f = open(log_file_temp,'a')
             f.write(sentence + end)
             f.close()
-        if(self.cout_tag):
+        if self.cout_tag:
             print(sentence,end=end)
-        if(end != '\n'):
+        if end != '\n':
             self.indent = 0
         else:
             self.indent = self.temp_indent
 
     def progress_bar(self,loop,loop_max,add_sentence=None,indent=0):
-        if(not self.silent):
+        if not self.silent:
             sys.stdout.write('\r')
             sentence = ''
-            if(indent > 0):
+            if indent > 0:
                 for i in range(indent):
                     sentence += '  ' 
             else:
@@ -91,11 +92,11 @@ class LogManager:
                     sentence += '  ' 
             progress_ratio = int(float(loop+1)/float(loop_max)*100)
             sentence += '[Progress : {0:0=3}%]'.format(progress_ratio)
-            if(add_sentence != None):
+            if add_sentence != None:
                 sentence += add_sentence
             sys.stdout.write(sentence)
             sys.stdout.flush()
-            if(progress_ratio >= 100):
+            if progress_ratio >= 100:
                 sys.stdout.write('\n')
 
 
